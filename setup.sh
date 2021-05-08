f47952f17df8df0286a32eed2466d1e15b28fa2f
faiz-crypto <80588143+faiz-crypto@users.noreply.github.com>
2021-05-08 19:55:05 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ sudo apt-add-repository ppa:fish-shell/release-3 -y
 sudo add-apt-repository ppa:kelleyk/emacs -y
 sudo add-apt-repository ppa:system76/pop -y
 
-sudo apt-get install -y git vim autojump fish emacs26 gnome-tweaks pop-theme build-essential exuberant-ctags flex bison htop exuberant-ctags
+sudo apt-get install -y git vim autojump fish emacs26 gnome-tweaks pop-theme build-essential exuberant-ctags flex bison htop exuberant-ctags curl
 
 # Install doom emacs
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
