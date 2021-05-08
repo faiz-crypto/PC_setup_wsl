f354fde7f514546e2c373e85c1b9c9fe21d497b3
faiz-crypto <80588143+faiz-crypto@users.noreply.github.com>
2021-05-08 17:02:28 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 sudo apt-get update && sudo apt-get upgrade -y
 
-sudo apt-add-repository ppa:fish-shell/release-3
-sudo add-apt-repository ppa:kelleyk/emacs
-sudo add-apt-repository ppa:system76/pop
+sudo apt-add-repository ppa:fish-shell/release-3 -y
+sudo add-apt-repository ppa:kelleyk/emacs -y
+sudo add-apt-repository ppa:system76/pop -y
 
-sudo apt-get install git vim autojump fish emacs26 gnome-tweaks pop-theme
+sudo apt-get install -y git vim autojump fish emacs26 gnome-tweaks pop-theme
